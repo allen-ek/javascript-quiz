b1fb458b330a18a2253f6a224ba38da0012acecd
A.Klein <allenkleined@gmail.com>
2022-12-13 14:10:58 -0800

    added highscores,quiz questions, and answerchecker

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
-# Random Password Generator Project
+# Javascript Quiz Project
 
 # Authors
 Allen Klein
-- [Link to Password Generator Site](https://allen-ek.github.io/javascript-quiz/)
+- [Link to Javscript Quiz Site](https://allen-ek.github.io/javascript-quiz/)
 - [Link to Github Repository](https://github.com/allen-ek/javascript-quiz)
+
+## Why?
+I wanted to create a Javascript quiz website that would be able to determine if the chosen answer was correct. Keep track of how many are correct to determine a score as well as save high scores to create a highscore list.
+## What I learned
+I learned various javascript methods and selectors to select certain html elements on the page   for dynamic functionality to an HTML webpage with CSS styling. I also learned how to create,modify and iteriate through arrays in javascript. I also learned how the user can make input selections and from there create different outputs based on the multiple choice question.
+
+## Technologies Used
+HTML
+JavaScript
+CSS
+Github
+## Code Snippet
+```html
+function quizQuestion(){
+    document.getElementById("gameover").style.display = "none";
+    
+    if (currQIndex === questions.length){
+        return displayScore();
+    } 
+    var currentQ = questions[currQIndex];
+    document.getElementById("questions").innerHTML = "<p>" + currentQ.question + "</p>";
+    document.getElementById("A").innerHTML = currentQ.option1;
+    document.getElementById("B").innerHTML = currentQ.option2;
+    document.getElementById("C").innerHTML = currentQ.option3;
+};
+```
+This code snippet showcases usage of the question prompt in javascript to be able to display onto the corresponding html page.
--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,164 @@
-
 //List of  quiz questions
 const questions=[{
-    question:"What does HTML stand for",
+    question:"What does HTML stand for?",
     option1:"Hyponotic Text Marked Language",
     option2:"Hyper Text Markup Language",
-    option3:"",
-    answer:""
+    option3:"Hyper Text Markdown Language",
+    answer:"B"
 },
 {
-    question:"What does CSS do",
+    question:"What does CSS control?",
     option1:"Create the webpage",
     option2:"Determine the styling of the page",
     option3:"It does nothing",
-    answer:""
+    answer:"B"
 },
 {
     question:"Which option is the divide operator in Javascript?",
     option1:"*",
     option2:"%",
     option3:"/",
-    answer:""
+    answer:"C"
 },
 {
-    question:"How do I create an array in Javascript",
-    option1:"Create the webpage",
-    option2:"Determine the styling of the page",
-    option3:"It does nothing",
-    answer:""
+    question:"How do I create an array in Javascript?",
+    option1:"const array=[]",
+    option2:"array =[]",
+    option3:"[][][]",
+    answer:"A"
 },
 {
     question:"How many JavaScript primitave data types are there?",
     option1:"5",
     option2:"6",
     option3:"7",
-    answer:"7"
-},
-{
-    question:"Which HTML element defines the title of a document?",
-    option1:"<title>",
-    option2:"<meta>",
-    option3:"<head>",
-    answer:""
+    answer:"C"
 },
 ]
 const timeEl =document.querySelector("#timer");
-startQuiz();
-// start quiz set inital state
+var totalTime=45;
+var timer;
+var score=0;
+var correct;
+var currQIndex=0;
+
+// start quiz once button is clicked
+document.getElementById("strtbtn").addEventListener("click",startQuiz);
+//sets intial state of the quiz
 function startQuiz(){
-    let timerId=setInterval(time,1000);
-    timeEl.textContent=500;
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("strtpage").style.display = "none";
+    quizQuestion();
+    //Timer function
+    timer=setInterval(function() {
+        totalTime--;
+        document.getElementById("timer").textContent = "Time" + totalTime;
+    
+        if(totalTime === 0) {
+          clearInterval(timer);
+          displayScore();
+        }
+      }, 1000);
+      document.getElementById("quiz").style.display = "block";
+}
+//function to display the questions
+function quizQuestion(){
+    document.getElementById("gameover").style.display = "none";
+    
+    if (currQIndex === questions.length){
+        return displayScore();
+    } 
+    var currentQ = questions[currQIndex];
+    document.getElementById("questions").innerHTML = "<p>" + currentQ.question + "</p>";
+    document.getElementById("A").innerHTML = currentQ.option1;
+    document.getElementById("B").innerHTML = currentQ.option2;
+    document.getElementById("C").innerHTML = currentQ.option3;
+};
+//function to check it the answer chosen is correct
+function checkAnswer(input){
+    correct = questions[currQIndex].answer;
+
+    if (input === correct && currQIndex !== questions.length){
+        score++;
+        alert("Correct!");
+        currQIndex++;
+        quizQuestion();  
+    }else if (input !== correct && currQIndex !== questions.length){
+        alert("Incorrect")
+        currQIndex++;
+        quizQuestion();
+    }else{
+        displayScore();
+    }
+}
+//score display
+function displayScore(){
+    document.getElementById("quiz").style.display = "none"
+    document.getElementById("gameover").style.display = "flex";
+    clearInterval(timer);
+    document.getElementById("initial").value="";
+    document.getElementById("finalScore").innerHTML = "You got " + score + " out of " + questions.length + " correct!";
+}
+//how to submit the quiz
+document.getElementById("submit").addEventListener("click", function highscore(){
+    if(document.getElementById("initial").value === "") {
+        alert("Enter Initals");
+        return false;
+    }else{
+        var savedHiscores = JSON.parse(localStorage.getItem("savedHiscores")) || [];
+        var currentUser = document.getElementById("initial").value.trim();
+        var currentHiscore = {
+            name : currentUser,
+            score : score
+    };
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("hiscoreContainer").style.display = "flex";
+    document.getElementById("hi-scorePage").style.display = "block";
+    document.getElementById("endGame").style.display = "flex";
+    savedHiscores.push(currentHiscore);
+    localStorage.setItem("savedHiscores", JSON.stringify(savedHiscores));
+    createHighscores();
+
+    }
+});
+//create the hiscore list
+function createHighscores(){
+    document.getElementById("hiscore-initials").innerHTML = "";
+    document.getElementById("hiscore-score").innerHTML = "";
+    var hiscores = JSON.parse(localStorage.getItem("savedHiscores")) || [];
+    for (i=0; i<hiscores.length; i++){
+        var name = document.createElement("li");
+        var score = document.createElement("li");
+        name.textContent = hiscores[i].name;
+        score.textContent = hiscores[i].score;
+        document.getElementById("hiscore-initials").appendChild(name);
+        document.getElementById("hiscore-score").appendChild(score);
+    }
+}
+//highscore display
+function displayHighscore(){
+    document.getElementById("strtpage").style.display = "none"
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("hiscoreContainer").style.display = "flex";
+    document.getElementById("hi-scorePage").style.display = "block";
+    document.getElementById("endGame").style.display = "flex";
+
+    createHighscores();
+}
+//clears the score
+function clearScore(){
+    window.localStorage.clear();
+    document.getElementById("hiscore-initials").textContent = "";
+    document.getElementById("hiscore-score").textContent = "";
+}
+//function to replay the quiz and reset values
+function replay(){
+    document.getElementById("hiscoreContainer").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("strtpage").style.display = "flex";
+    totalTime = 45;
+    score = 0;
+    currQIndex=0;
 }
 
-function quizQuestion(){
-    var currQIndex=0;
-    const overDiv = document.getElementById("end");
-    gameoverDiv.style.display = "none";
-    if (currQIndex === endQIndex){
-        return showScore();
-    } 
-    var currentQuestion = quizQuestions[currentQuestionIndex];
-    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
-    buttonA.innerHTML = currentQ.choiceA;
-    buttonB.innerHTML = currentQ.choiceB;
-    buttonC.innerHTML = currentQ.choiceC;
-    buttonD.innerHTML = currentQ.choiceD;
-};
 
